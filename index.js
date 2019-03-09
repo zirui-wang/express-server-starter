d5aa44b019fe75694e3ba2dc4787da5f0e97c085
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2019-03-08 17:55:43 -0800

    feat: use socket.io build simple chat room

--- index.js
+++ index.js
@@ -9,9 +9,18 @@ app.use(bodyParser.json());
 // CORS.
 app.use(cors());
 
-app.get('/users', (req, res) => {
-  res.send("");
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+  socket.on('chat message', function(msg) {
+    io.emit('chat message', msg);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log('Listening on ' + PORT));
+server.listen(PORT, () => console.log('Listening on ' + PORT));
