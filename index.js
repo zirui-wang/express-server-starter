e73470b4ca6f0abd74f002c3a5ca997a2ad07172
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2019-03-08 17:20:46 -0800

    Refactor.

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express');
+const app = require('express')();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-const app = express();
-
+// Parse imcoming request bodies and enable req.body.
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// CORS.
 app.use(cors());
 
 app.get('/users', (req, res) => {
