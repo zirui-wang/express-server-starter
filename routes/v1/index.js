38797ec63f6e1d455f5c891da27be4c975882461
zirui-wang <zirui.zirui.wang.wang@gmail.com>
2019-03-12 20:39:33 -0700

    feat: scan routes in folder automatically.

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,9 +3,10 @@
 const v1 = require('express').Router();
 
 const router = require('../router');
+const scanRoutes = require('../scan-routes');
 
 module.exports = app => {
-  const routes = [require('./greeting'), require('./json')];
+  const routes = scanRoutes(__dirname)
   v1.use('/', router(routes, app));
   return v1;
 };
